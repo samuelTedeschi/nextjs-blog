5a2b949dd045a275f7e22cad0dab296845254133
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-08-13 22:59:54 -0300

    .

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,17 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from "../../components/layout";
 
-export default function FirstPost(){
-    return(
-        <>
-        <h1>Primero Post</h1>
-        <h2>
-            <Link href="/">voltar para a home</Link>
-        </h2>
-        </>
-    ) 
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>Primero Post</title>
+            </Head>
+            <h1>Primero Post</h1>
+            <h2>
+                <Link href="/">voltar para a home</Link>
+            </h2>
+        </Layout>
+    )
 }
