7de8834831c82b7e87e0286e1e5ddcf18b5bf345
Samuel Tedeschi <samuel_tedeschi@yahoo.com>
2023-08-17 11:54:02 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date'
+import Link from 'next/link'
 
 
 export default function Home({ allPostsData }) {
@@ -13,23 +15,22 @@ export default function Home({ allPostsData }) {
         </title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Novidade sobre Tecnologis</p>
-        <p>
-          (Blog onde irei colocar as novidades da semana e tutoriais{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Ola! Eu sou o Samuel e sou front end developer</p>
+        <p>Nesse Blog eu vou postar informaçoes sobre tecnologias e produtividade. Tenha uma boa leitura!</p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
+            <li className={ `${utilStyles.listItem} ${utilStyles.listItemPadding}`} key={id}>
+              <Link href={`/posts/${id}`}>
               {title}
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
             </li>
           ))}
         </ul>
